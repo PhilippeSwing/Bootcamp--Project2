f51d2fde5c2cc8803a4626df765e478a74b87895
Phil <bourassa.phil@gmail.com>
2018-08-12 00:58:02 -0400

    Added on-page-load header animation. Images aren't loading for flickity. Need to troubleshoot further.

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.client-carriage__image-container').flickity({
+        autoPlay: false,
+        pauseAutoPlayOnHover: false,
+        lazyLoad: true,
+        freeScroll: false,
+        prevNextButtons: true,
+        pageDots: false,
+        accessibility: true
+    });
+});
